eef741226781965e6933e642eb90318e5e82e893
bigLRRH <51252247+bigLRRH@users.noreply.github.com>
2023-05-25 23:44:54 +0800

    updated csp_test

--- P5662.cpp
+++ P5662.cpp
@@ -6,12 +6,22 @@ int main()
 {
     int T, N, M;
     cin >> T >> N >> M;
-    array<int,100> P = {0};
-    for (int i = 0; i < T; ++i)
+    array<int, 100> W={}, P={}; 
+    for (int j = 0; j < N; ++j)
+        cin>>W[j];
+    for (int t = 1; t < T; ++t)
     {
-        for (int j = 0; j < N; ++j)
-            cin >> P[j];
-        
+        array<int,10001> dp={};
+        for(int i=0;i<N;++i)
+        {
+            cin >> P[i];
+            for(int j=W[i];j<=M;++j)
+            {
+                dp[j]=max(dp[j],dp[j-W[i]]+P[i]-W[i]);
+            }
+        }
+        M +=dp[M];
+        W = move(P);
     }
     cout << M << endl;
     return 0;
---
+++ P5663.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n,m,q;
+    array<vector<int>,100000> g;
+    cin>>n>>m>>q;
+    for(int i=0;i<m;++i)
+    {
+        int u,v;
+        cin>>u>>v;
+
+    }
+    for(int i=0;i<q;++q)
+    {
+
+    }
+}
